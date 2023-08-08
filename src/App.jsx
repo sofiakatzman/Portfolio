622ae2ac603b1fefc68005bce45c4abc6f379db6
Sofia <katzmansof@gmail.com>
2023-08-08 16:00:24 -0400

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './components/Navigation'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Navigation />
+      Testing
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navigation.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Navigation(){
+    return(
+        <>
+        Testing
+        </>
+    )
+}
+
+export default Navigation
