0d3300b219ef6412b3f5276ff8b8f78a14c4b3b7
Sofia <katzmansof@gmail.com>
2023-08-10 17:11:41 -0400

    update styling

--- src/components/Pages/About/About.js
+++ src/components/Pages/About/About.js
@@ -5,7 +5,7 @@ import headshot from '../../../assets/headshot.jpg'
 function About(){
 
     return(
-
+        <div className="about">
             <div className="about-container">
                 <div className="about-headshot">
                     <img src={headshot} alt="Headshot" className="circle-image" />
@@ -13,11 +13,12 @@ function About(){
                 <div className="about-blurb">
                     <h2>Full Stack Web Developer</h2>
                     <h3>New York, NY </h3>
-                    <p>Hello! My name is Sofia, an artist turned software engineer.</p>
+                    <p>Hello! I'm Sofia, an artist turned software engineer.</p>
                     <p>With my diverse background in operations, logistics, and design, I am able to craft innovative and eclectic solutions for all web development needs. </p>  
                 </div>
                 
             </div>
+        </div>
 
   
     )
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,10 +1,12 @@
 import React from "react"
 import About from "./About/About"
+import Technologies from "./Technologies/Technologies"
 
 function Home(){
     return(
         <div>
             <About /> 
+            <Technologies />
         </div>
     )
 }
