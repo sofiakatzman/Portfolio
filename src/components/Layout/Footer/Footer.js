4cf35d9240cade57cdca1f7494f665b6b0332aa3
Sofia <katzmansof@gmail.com>
2023-08-10 13:48:58 -0400

    add footer styling and logic

---
+++ src/components/Layout/Footer/Footer.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Contact from "../Contact/Contact"
+import "./Footer.css"
+
+
+function Footer(){
+    return(
+        <div className="footer">
+        <Contact /> 
+        </div>
+    )
+}
+
+export default Footer
