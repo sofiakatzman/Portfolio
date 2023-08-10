d7b4988e9e954cda6de9211043163f4c357bf0df
Sofia <katzmansof@gmail.com>
2023-08-10 13:49:40 -0400

    add about me

---
+++ src/components/Pages/About/About.js
@@ -0,0 +1,26 @@
+import React from "react"
+import './About.css'
+import headshot from '../../../assets/headshot.jpg'
+
+function About(){
+
+    return(
+
+            <div className="about-container">
+                <div className="about-headshot">
+                    <img src={headshot} alt="Headshot" className="circle-image" />
+                </div>
+                <div className="about-blurb">
+                    <h2>Full Stack Web Developer</h2>
+                    <h3>New York, NY </h3>
+                    <p>Hello! My name is Sofia, an artist turned software engineer.</p>
+                    <p>With my diverse background in operations, logistics, and design, I am able to craft innovative and eclectic solutions for all web development needs. </p>  
+                </div>
+                
+            </div>
+
+  
+    )
+}
+
+export default About
--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -1,10 +1,11 @@
 import React from "react"
+import About from "./About/About"
 
 function Home(){
     return(
-        <>
-        This is Home
-        </>
+        <div>
+            <About /> 
+        </div>
     )
 }
 
