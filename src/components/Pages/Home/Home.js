b344e7689348499ec6ce90420c93a109093562c7
Sofia <katzmansof@gmail.com>
2023-08-28 05:37:31 -0400

    move home

--- src/components/Pages/Home/Home.js
+++ src/components/Pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from "react"
-import About from "./About/About"
-import Technologies from "./Technologies/Technologies"
+import About from "../About/About"
+import Technologies from "../Technologies/Technologies"
+import Projects from "../Projects/Projects"
 
 function Home(){
     return(
         <div>
             <About /> 
             <Technologies />
+            <Projects />
         </div>
     )
 }
