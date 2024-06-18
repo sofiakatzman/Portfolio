b945be34e2511ad164335ccec40b109832f93e5b
Sofia Katzman <katzmansof@gmail.com>
2024-06-17 22:36:46 -0400

    prettier formatted

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,39 +1,40 @@
-import React from 'react';
-import design_icon from '../assets/icons/design_icon.png';
-import dev_icon from '../assets/icons/development_icon.png';
-import ops_icon from '../assets/icons/ops_icon.png';
+import React from "react";
+import design_icon from "../assets/icons/design_icon.png";
+import dev_icon from "../assets/icons/development_icon.png";
+import ops_icon from "../assets/icons/ops_icon.png";
 
 function Skills() {
-    const skills = [
-        {
-            title: 'Design',
-            icon: design_icon,
-            alt: 'design icon - pencil',
-        },
-        {
-            title: 'Dev',
-            icon: dev_icon,
-            alt: 'development icon - mouse cursor',
-        },
-        {
-            title: 'Ops',
-            icon: ops_icon,
-            alt: 'operations icon - magic wand',
-        },
-    ];
+  const skills = [
+    {
+      title: "Design",
+      icon: design_icon,
+      alt: "design icon - pencil",
+    },
+    {
+      title: "Dev",
+      icon: dev_icon,
+      alt: "development icon - mouse cursor",
+    },
+    {
+      title: "Ops",
+      icon: ops_icon,
+      alt: "operations icon - magic wand",
+    },
+  ];
 
-    return (
-        <div>
-            <div className="skills-container">
-                {skills.map((skill, index) => (
-                    <button key={index} className="skill-button">
-                        <img src={skill.icon} alt={skill.alt} className="skill-icon" />
-                        <span>{skill.title}</span>
-                    </button>
-                ))}
-            </div>
-        </div>
-    );
+  return (
+    <div id="skills">
+      <h2>Skills</h2>
+      <div className="skills-container">
+        {skills.map((skill, index) => (
+          <button key={index} className="skill-button">
+            <img src={skill.icon} alt={skill.alt} className="skill-icon" />
+            <span>{skill.title}</span>
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Skills;
