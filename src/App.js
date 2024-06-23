29e67facd46738e36860fd0fce571375a54ad1d1
Sofia Katzman <katzmansof@gmail.com>
2024-06-23 17:29:20 -0400

    add new components - works dev,design,loadscreen

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Services from "./components/Services";
 import "./App.css";
 import Footer from "./components/Footer";
 import Signoff from "./components/Signoff";
 import MyStory from "./components/MyStory";
 import LoadingScreen from "./components/LoadingScreen"; // Import the LoadingScreen component
+import DesignWorks from "./components/DesignWorks";
+import DevWorks from "./components/DevWorks";
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    // Simulate loading delay (you can replace this with actual data fetching logic)
-    setTimeout(() => {
-      setLoading(false); // Set loading to false after 2 seconds (simulating data loading)
-    }, 2000);
-  }, []);
+  // useEffect(() => {
+  //   setTimeout(() => {
+  //     setLoading(false);
+  //   }, 1000);
+  // }, []);
 
   return (
     <div>
@@ -25,6 +27,9 @@ function App() {
         ) : (
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/work/design" element={<DesignWorks />} />
+            <Route path="/work/dev" element={<DevWorks />} />
             <Route path="/story" element={<MyStory />} />
           </Routes>
         )}
