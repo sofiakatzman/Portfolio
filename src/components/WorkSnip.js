74c81e84e1c9864dcbd065392b03528d57ec61d7
Sofia Katzman <katzmansof@gmail.com>
2024-06-23 17:28:22 -0400

    instansiate design works page

--- src/components/WorkSnip.js
+++ src/components/WorkSnip.js
@@ -1,18 +1,22 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import design_icon from "../assets/work-icon/design_worksnip.png";
 import dev_icon from "../assets/work-icon/development_worksnip.png";
 
 function WorkSnip() {
+  // Assuming you have paths set up for DesignWorks and DevWorks components
   const work = [
     {
       title: "Design",
       icon: design_icon,
       alt: "design icon - pencil",
+      path: "/work/design",
     },
     {
       title: "Dev",
       icon: dev_icon,
       alt: "development icon - mouse cursor",
+      path: "/work/dev",
     },
   ];
 
@@ -21,11 +25,15 @@ function WorkSnip() {
       <div className="worksnip" id="work">
         <h1 className="small">My Work</h1>
         <div className="work-container">
-          {work.map((work, index) => (
-            <button key={index} className="work-button">
-              <img src={work.icon} alt={work.alt} className="work-icon" />
-              <p className="bold">{work.title}</p>
-            </button>
+          {work.map((workItem, index) => (
+            <Link key={index} to={workItem.path} className="work-button">
+              <img
+                src={workItem.icon}
+                alt={workItem.alt}
+                className="work-icon"
+              />
+              <p className="bold">{workItem.title}</p>
+            </Link>
           ))}
         </div>
       </div>
