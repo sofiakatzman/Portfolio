18eb31b340672ef256ce0fb6c7c6b7957783587f
Sofia Katzman <katzmansof@gmail.com>
2024-06-22 20:37:47 -0400

    style and prep data structure - skill technologies

--- src/assets/skills.js
+++ src/assets/skills.js
@@ -2,6 +2,31 @@ import design_icon from "./icons/design_icon.png";
 import dev_icon from "./icons/development_icon.png";
 import ops_icon from "./icons/ops_icon.png";
 
+// design icons
+import technology_adobe_icon from "../assets/technologies/design/Adobe.svg";
+import technology_blender_icon from "../assets/technologies/design/Blender.svg";
+import technology_Procreate_icon from "../assets/technologies/design/Procreate.svg";
+import technology_ProcreateDreams_icon from "../assets/technologies/design/ProcreateDreams.svg";
+import technology_figma_icon from "../assets/technologies/design/Figma.svg";
+import technology_invision_icon from "../assets/technologies/design/Invision.svg";
+
+// // dev icons
+// import technology_js_icon from "../assets/technologies/dev/JavaScript.svg";
+// import technology_react_icon from "../assets/technologies/dev/React.svg";
+// import technology_node_icon from "../assets/technologies/design/NodeJS.svg";
+// import technology_python_icon from "../assets/technologies/design/Python.svg";
+// import technology_ts_icon from "../assets/technologies/design/Typescript.svg";
+// import technology_sql_icon from "../assets/technologies/design/SQL.svg";
+// import technology_flask_icon from "../assets/technologies/design/Flask.svg";
+
+// // ops icons
+// import technology_zapier_icon from "../assets/technologies/ops/zapier.svg";
+// import technology_sf_icon from "../assets/technologies/ops/salesforce.svg";
+// import technology_at_icon from "../assets/technologies/ops/airtable.svg";
+// import technology_bc_icon from "../assets/technologies/ops/basecamp.svg";
+// import technology_im_icon from "../assets/technologies/ops/inventorymanagement.svg";
+// import technology_excel_icon from "../assets/technologies/ops/excel.svg";
+
 const skills = [
   {
     title: "Design",
@@ -27,25 +52,28 @@ const skills = [
       header2: "Technologies",
       technologies: [
         {
-          name: "Adobe Creative Suite / Cloud",
-          icon: "../assets/technologies/design/Adobe.svg",
+          name: "Adobe Suite",
+          icon: technology_adobe_icon,
         },
         {
           name: "Blender",
-          icon: "../assets/technologies/design/Blender.svg",
+          icon: technology_blender_icon,
         },
         {
           name: "Procreate",
-          icon: "../assets/technologies/design/Procreate.svg",
+          icon: technology_Procreate_icon,
         },
         {
           name: "Procreate Dreams",
-          icon: "../assets/technologies/design/ProcreateDreams.svg",
+          icon: technology_ProcreateDreams_icon,
         },
-        { name: "Figma", icon: "../assets/technologies/design/Figma.svg" },
+        {
+          name: "Figma",
+          icon: technology_figma_icon,
+        },
         {
           name: "Invision",
-          icon: "../assets/technologies/design/Invision.svg",
+          icon: technology_invision_icon,
         },
       ],
     },
@@ -74,31 +102,31 @@ const skills = [
       technologies: [
         {
           name: "JavaScript",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "React.js",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "Node.js",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "Python",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "Typescript",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "SQL",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "Flask",
-          icon: "",
+          icon: technology_adobe_icon,
         },
       ],
     },
@@ -127,28 +155,28 @@ const skills = [
       technologies: [
         {
           name: "Zapier",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "Salesforce",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "Airtable",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "Basecamp",
-          icon: "",
+          icon: technology_adobe_icon,
         },
 
         {
           name: "Inventory Management",
-          icon: "",
+          icon: technology_adobe_icon,
         },
         {
           name: "Excel",
-          icon: "",
+          icon: technology_adobe_icon,
         },
       ],
     },
@@ -156,3 +184,162 @@ const skills = [
 ];
 
 export default skills;
+
+// saving for when the svgs for each icon are uploaded - to bypass react error
+// const skills = [
+//   {
+//     title: "Design",
+//     icon: design_icon,
+//     alt: "design icon - pencil",
+//     data: {
+//       title: "Design",
+//       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed finibus lorem.",
+//       header1: "Professional Experience",
+//       experience: [
+//         "Graphic Design",
+//         "Web Design",
+//         "Mobile Design",
+//         "Illustration",
+//         "Fabrication",
+//         "Unifying Branding",
+//         "Asset Auditing",
+//         "Templatization",
+//         "Adobe Scripting",
+//         "Design Systems",
+//         "Asset Management",
+//       ],
+//       header2: "Technologies",
+//       technologies: [
+//         {
+//           name: "Adobe Suite",
+//           icon: technology_adobe_icon,
+//         },
+//         {
+//           name: "Blender",
+//           icon: technology_blender_icon,
+//         },
+//         {
+//           name: "Procreate",
+//           icon: technology_Procreate_icon,
+//         },
+//         {
+//           name: "Procreate Dreams",
+//           icon: technology_ProcreateDreams_icon,
+//         },
+//         {
+//           name: "Figma",
+//           icon: technology_figma_icon,
+//         },
+//         {
+//           name: "Invision",
+//           icon: technology_invision_icon,
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     title: "Dev",
+//     icon: dev_icon,
+//     alt: "development icon - mouse cursor",
+//     data: {
+//       title: "Development",
+//       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed finibus lorem.",
+//       header1: "Professional Experience",
+//       experience: [
+//         "Frontend Development",
+//         "Backend Development",
+//         "Full-stack Development",
+//         "Web Development",
+//         "Mobile App Development",
+//         "Database Management",
+//         "Version Control (Git)",
+//         "API Integration",
+//         "Testing & Debugging",
+//         "Deployment & Maintenance",
+//       ],
+//       header2: "Technologies",
+//       technologies: [
+//         {
+//           name: "JavaScript",
+//           icon: technology_js_icon,
+//         },
+//         {
+//           name: "React.js",
+//           icon: technology_react_icon,
+//         },
+//         {
+//           name: "Node.js",
+//           icon: technology_node_icon,
+//         },
+//         {
+//           name: "Python",
+//           icon: technology_python_icon,
+//         },
+//         {
+//           name: "Typescript",
+//           icon: technology_ts_icon,
+//         },
+//         {
+//           name: "SQL",
+//           icon: technology_sql_icon,
+//         },
+//         {
+//           name: "Flask",
+//           icon: technology_flask_icon,
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     title: "Ops",
+//     icon: ops_icon,
+//     alt: "operations icon - magic wand",
+//     data: {
+//       title: "Operations",
+//       body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus sed finibus lorem.",
+//       header1: "Professional Experience",
+//       experience: [
+//         "Process Automation",
+//         "Operational Flow Creation",
+
+//         "Compliance & Auditing",
+//         "Office Operations",
+//         "Sales Operations",
+//         "Freight Forwarding",
+//         "Logistics",
+//         "Event Management",
+//         "Capacity Planning",
+//       ],
+//       header2: "Technologies",
+//       technologies: [
+//         {
+//           name: "Zapier",
+//           icon: technology_zapier_icon,
+//         },
+//         {
+//           name: "Salesforce",
+//           icon: technology_sf_icon,
+//         },
+//         {
+//           name: "Airtable",
+//           icon: technology_at_icon,
+//         },
+//         {
+//           name: "Basecamp",
+//           icon: technology_bc_icon,
+//         },
+
+//         {
+//           name: "Inventory Management",
+//           icon: technology_im_icon,
+//         },
+//         {
+//           name: "Excel",
+//           icon: technology_excel_icon,
+//         },
+//       ],
+//     },
+//   },
+// ];
+
+// export default skills;
--- src/components/PopupContent.js
+++ src/components/PopupContent.js
@@ -1,30 +1,43 @@
 import React from "react";
 import skills from "../assets/skills";
+import bullet from "../assets/bulletpoint.svg";
+import Technology from "./Technology";
 
 function PopupContent({ content }) {
   const skill = skills.find((skill) => skill.title.toString() === content);
 
   if (!skill) {
-    return <div>Skill not found</div>;
+    return (
+      <div>Sorry, there's an error on our end. Send me a message instead!</div>
+    );
   }
 
   return (
-    <div>
+    <div className="content">
       <h2>{skill.data.title}</h2>
       <p>{skill.data.body}</p>
       <h3>{skill.data.header1}</h3>
-      <ul>
-        {skill.data.experience.map((exp, index) => (
-          <li key={index}>{exp}</li>
-        ))}
-      </ul>
-      <h3>{skill.data.header2}</h3>
-      <ul>
-        {Array.isArray(skill.data.technologies) &&
-          skill.data.technologies.map((tech, index) => (
-            <li key={index}>{tech.name}</li>
+      <div className="content-container">
+        <ul className="content-list">
+          {skill.data.experience.map((exp, index) => (
+            <li key={index} className="bullet-list-item small">
+              <img src={bullet} alt="Bullet Point" />
+              {exp}
+            </li>
           ))}
-      </ul>
+        </ul>
+      </div>
+      <h3>{skill.data.header2}</h3>
+      <div className="technologies-container">
+        <ul className="content-list">
+          {Array.isArray(skill.data.technologies) &&
+            skill.data.technologies.map((tech, index) => (
+              <li key={index} className="technologies">
+                <Technology name={tech.name} icon={tech.icon} />
+              </li>
+            ))}
+        </ul>
+      </div>
     </div>
   );
 }
