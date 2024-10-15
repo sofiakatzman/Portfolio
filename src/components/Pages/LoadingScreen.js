a45e0351c2cea6bde41a2de3e9696d8c7c66e175
Sofia <katzmansof@gmail.com>
2024-10-14 21:32:18 -0400

    remove styling from inside component

--- src/components/Pages/LoadingScreen.js
+++ src/components/Pages/LoadingScreen.js
@@ -1,21 +1,9 @@
 import React from "react";
+import "./styles.css"
 
 const LoadingScreen = () => {
   return (
-    <div
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        background: "#fff",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        zIndex: 9999,
-      }}
-    >
+    <div className="loading-screen" >
       <h2>LOADING...</h2>
     </div>
   );
