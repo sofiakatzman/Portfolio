657e3f89d5237119332cfefca90576b2cf681e4a
Sofia <katzmansof@gmail.com>
2024-10-23 00:16:23 -0400

    update styling of projects

--- src/components/Organisms/Projects.js
+++ src/components/Organisms/Projects.js
@@ -1,9 +1,10 @@
 import { useState } from "react"
 import { projectsData } from "../../data/projectsData.js"
+import "./Project.css"
+import WorkCard from "../Atoms/WorkCard.js"
 
 function Projects(){
     const projectCount=projectsData.length
-    console.log(projectCount)
 
     const [projIndex, setProjIndex] = useState(0)
 
@@ -24,16 +25,26 @@ function Projects(){
     }
 
     return(
-         <div>
-            <h2>{projectsData[projIndex].name}</h2>
-            <img
-                src={projectsData[projIndex].image}
-                alt={projectsData[projIndex].name}
-            />
-            <p>{projectsData[projIndex].description}</p>
-            <button onClick={goBackward}>backward</button>
-            {projIndex}
-            <button onClick={goForward}>forward</button>
+         <div className="project-container" id="work" >
+            <div className="carousel-container">
+                <button onClick={goBackward} className="proj-arrow back">◄</button>
+                <img
+                    src={projectsData[projIndex].image}
+                    alt={projectsData[projIndex].name}
+                    className="proj-img"
+                />
+                <button onClick={goForward} className="proj-arrow next">►</button>
+            </div>
+                <WorkCard 
+                    title = {projectsData[projIndex].name} 
+                    content ={projectsData[projIndex].description}
+                    tags = {projectsData[projIndex].tags}    
+                    altText= {projectsData[projIndex].name}
+                    cta1 = {projectsData[projIndex].cta1_url}
+                    cta1Copy = {projectsData[projIndex].cta1_copy}
+                    cta2 = {projectsData[projIndex].cta2_url}
+                    cta2Copy = {projectsData[projIndex].cta2_copy}
+                />
         </div>
     )
 }
