7d3135fd6cb544cb5446f3951d4e44b64a94cac2
Sofia <katzmansof@gmail.com>
2024-10-23 00:17:34 -0400

    replace workcard styling and downgrade og

--- src/components/Atoms/WorkCard.js
+++ src/components/Atoms/WorkCard.js
@@ -1,21 +1,27 @@
 import "./style.css"
-function WorkCard({ title, content, tags, link, img, altText }) {
+function WorkCard({ title, content, tags, cta1, cta2, cta1Copy, cta2Copy, img, altText }) {
     return (
-        <div className="border-shadow work-card">
+        <div className="border-noshadow work-card">
             <div className="flex-container space-between">
-            <h2>{title}</h2>
-            <div className="flex-container label-container">
-                {tags.map((tag, index) => (
-                    <span key={index} className="label">{tag}</span>
-                ))}
+                <h2>{title}</h2>
+                    <div className="flex-container label-container">
+                        {tags.map((tag, index) => (
+                            <span key={index} className="label">{tag}</span>
+                        ))}
+                    </div>
             </div>
-            </div>
-            ---------------------------------------------
+            <hr className="dashed-line" />
+
             <div className="flex-container">
-            <p className="xsm">{content}</p>
-            <img src={img} alt={altText} className="work-imgs"/>
+                <p className="xsm">{content}</p>
+                {img && <img src={img} alt={altText} className="work-imgs"/>}
             </div>
-            <a href={link}><p className="xsm"><b>Read More</b></p></a>
+            <div className="flex-container">
+                <a href={cta1}><p className="xsm"><b>{cta1Copy} </b></p></a>
+                <a href={cta2}><p className="xsm  lp"><b> {cta2Copy}</b></p></a>
+            </div>
+
+
 
         </div>
     );
