05c75f7a27e7d3aaa0452302f02ebf401daf2051
Sofia <katzmansof@gmail.com>
2024-10-24 18:47:29 -0400

    add contact form

--- src/components/Pages/Work/AssetGenerator.js
+++ src/components/Pages/Work/AssetGenerator.js
@@ -3,6 +3,9 @@ import { projectsData } from "../../../data/projectsData.js"
 import Workflow from "../../Organisms/Modules/Workflow.js"
 import { useEffect } from "react";
 import InfoSteps from "../../Organisms/Modules/InfoSteps.js";
+import ContactForm from "../../Organisms/ContactForm.js";
+import Footer from "../../Organisms/Footer.js";
+
 
 function AssetGenerator(){
     useEffect(() => {
@@ -17,6 +20,8 @@ function AssetGenerator(){
             <Description project={projectsData[1]}/>
             <Workflow project={projectsData[1]}/>
             <InfoSteps project={projectsData[1]}/>
+            <ContactForm />
+            <Footer />
         </div>
     )
 }
