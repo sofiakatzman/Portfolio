56a2b4298590b2e25be05c9e538260be2034d745
Sofia <katzmansof@gmail.com>
2024-10-24 18:16:18 -0400

    add key

--- src/components/Atoms/StackedLogo.js
+++ src/components/Atoms/StackedLogo.js
@@ -2,7 +2,7 @@ import "./style.css"
 function StackedLogo({tool}){
     return(
         <div className="sl">
-            <img src={tool.logo}/><br></br>
+            <img src={tool.logo} alt={tool}/><br></br>
             {/* <strong>{tool.name}</strong> */}
             <p className="xsm">{tool.description}</p>
         </div>
